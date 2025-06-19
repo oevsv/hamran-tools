b14f57532de7652e4f3db0bb453a1a4bc64aa440
Roland Schwarz OE1RSA <roland.schwarz@blackspace.at>
2025-06-19 19:57:42 +0200

    use rang lib from streampu

--- apps/lm2-gpio.cpp
+++ apps/lm2-gpio.cpp
@@ -4,17 +4,19 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 //
 
-
 #include "gitflow.hpp"
-#include <limesuiteng/limesuiteng.hpp>
 #include <CLI/CLI.hpp>
+#include <limesuiteng/limesuiteng.hpp>
+
+#include <rang.hpp>
+using rang::style, rang::fgB, rang::fg;
 
 #include <iostream>
 using std::cout, std::endl;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    cout << style::bold << fgB::red << "Hello World!" << fg::reset << style::reset << endl;
     cout << gitflow::tag << endl;
     return 0;
 }
