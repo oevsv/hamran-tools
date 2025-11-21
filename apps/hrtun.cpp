c78b99fc01b8578c5990c5f9d696820f8b619a8f
Roland Schwarz OE1RSA <roland.schwarz@blackspace.at>
2025-11-21 13:45:53 +0100

    Add exploratory tun code, add FLASH tool

--- apps/hrtun.cpp
+++ apps/hrtun.cpp
@@ -14,18 +14,63 @@ using spdlog::trace, spdlog::debug, spdlog::info, spdlog::warn, spdlog::error,
 
 #include <cstdlib>
 
+#include <cstddef>
+using std::byte, std::to_integer;
+
 #include <iostream>
 using std::cin, std::cout, std::cerr, std::endl, std::flush;
 
 #include <string>
 using std::string, std::getline;
 
-#include <stdexcept>
+#include <vector>
+using std::vector;
+
+#include <exception>
 using std::exception;
+
+#include <csignal>
+using std::sig_atomic_t, std::signal;
+
+namespace {
+
+volatile sig_atomic_t signal_status = 0;
+void signal_handler(int signal) { signal_status = signal; }
+
+extern "C" void tuntapLogHandler(int level, const char *errmsg) {
+
+  switch (level) {
+  case TUNTAP_LOG_ERR:
+    spdlog::critical("libtuntap error: {}", errmsg);
+    break;
+  case TUNTAP_LOG_WARN:
+    spdlog::error("libtuntap warn: {}", errmsg);
+    break;
+  case TUNTAP_LOG_NOTICE:
+    spdlog::warn("libtuntap notice: {}", errmsg);
+    break;
+  case TUNTAP_LOG_INFO:
+    spdlog::info("libtuntap: {}", errmsg);
+    break;
+  case TUNTAP_LOG_DEBUG:
+    spdlog::debug("libtuntap: {}", errmsg);
+    break;
+  case TUNTAP_LOG_NONE:
+  default:
+    spdlog::trace("libtuntap: {}", errmsg);
+    break;
+  }
+}
+
+} // namespace
 
 int main(int argc, char **argv) {
 
   int main_exit = EXIT_SUCCESS;
+
+  signal(SIGINT, signal_handler);
+  signal(SIGTERM, signal_handler);
+
   string loglevel = "info";
 
   try {
@@ -33,17 +78,42 @@ int main(int argc, char **argv) {
     spdlog::flush_on(spdlog::get_level());
 
     tuntap::tuntap tun(TUNTAP_MODE_TUNNEL);
+    tuntap_log_set_cb(tuntapLogHandler);
 
-    cout << "interactive mode, enter message or EOF "
-            "(Ctrl-D) or empty "
-         << endl;
-    string line;
-    while (getline(cin, line)) {
-      if (line.empty())
-        break;
-      cout << line << endl;
-      cout << "> " << flush;
+    info("name: {}", tun.name());
+    info("mtu: {}", tun.mtu());
+    tun.ip("10.23.3.0", 24);
+
+    tun.up();
+
+    vector<byte> rx_buf(tun.mtu());
+
+    while (0 == signal_status) {
+      int rc = tun.read(rx_buf.data(), rx_buf.size(), 1000);
+      if (-1 < rc) {
+        cout << "Size: " << rc << ", ";
+        int vers = to_integer<unsigned>(rx_buf[0] >> 4);
+        if (4 == vers) {
+          cout << "VERS: " << 4 << ", ";
+          cout << "HLEN: " << to_integer<int>(byte{0x0f} & rx_buf[0]) << ", ";
+          cout << "SRC: " << to_integer<unsigned>(rx_buf[12]) << "."
+               << to_integer<unsigned>(rx_buf[13]) << "."
+               << to_integer<unsigned>(rx_buf[14]) << "."
+               << to_integer<unsigned>(rx_buf[15]) << ", ";
+          cout << "DST: " << to_integer<unsigned>(rx_buf[16]) << "."
+               << to_integer<unsigned>(rx_buf[17]) << "."
+               << to_integer<unsigned>(rx_buf[18]) << "."
+               << to_integer<unsigned>(rx_buf[19]) << ", ";
+        } else if (6 == vers) {
+          cout << "VERS: " << 6 << ", ";
+        } else {
+          cout << "VERS: " << vers << endl;
+        }
+        cout << "end" << endl;
+      }
     }
+
+    tun.down();
 
   } catch (const exception &e) {
     critical("{}", e.what());
