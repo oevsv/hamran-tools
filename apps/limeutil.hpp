5c08cf41f0b6880a4cae18e095160aced04d2d06
Roland Schwarz OE1RSA <roland.schwarz@blackspace.at>
2025-12-07 13:24:15 +0100

    Add hrbeacon app

--- apps/limeutil.hpp
+++ apps/limeutil.hpp
@@ -13,46 +13,31 @@
 namespace lime {
 namespace util {
 
-template <std::uintmax_t SAMPLERATE> class stream_clock;
-
-template <std::uintmax_t SAMPLERATE, class Duration>
-using stream_time = std::chrono::time_point<stream_clock<SAMPLERATE>, Duration>;
-
 template <std::uintmax_t SAMPLERATE> class stream_clock {
 public:
   typedef int64_t rep;
   typedef std::ratio<1l, SAMPLERATE> period;
   typedef std::chrono::duration<rep, period> duration;
-  typedef std::chrono::time_point<stream_clock> time_point;
+  typedef std::chrono::time_point<std::chrono::system_clock, duration>
+      time_point;
   static constexpr bool is_steady = false;
 
-  // stream_clock(const lime::RFStream &stream)
-  //     : stream(stream), sys_epoch(std::chrono::time_point_cast<duration>(
-  //                                     std::chrono::system_clock::now()) -
-  //                                 duration(stream.GetHardwareTimestamp())) {}
-
   stream_clock(const lime::RFStream &stream)
-      : stream(stream), sys_epoch(std::chrono::system_clock::now() -
+      : stream(stream), sys_epoch(std::chrono::time_point_cast<duration>(
+                                      std::chrono::system_clock::now()) -
                                   duration(stream.GetHardwareTimestamp())) {}
 
   time_point now() noexcept {
-    return duration(stream.GetHardwareTimestamp()) + time_point();
+    return duration(stream.GetHardwareTimestamp()) + sys_epoch;
   }
 
-  template <class Duration>
-  static auto from_sys(std::chrono::sys_time<Duration> const &tp) noexcept {
-    return stream_time(tp);
-  }
-
-  template <class Duration>
-  static auto to_sys(stream_time<SAMPLERATE, Duration> const &tp) noexcept {
-    return std::chrono::system_clock::time_point(); // + tp;
+  lime::Timespec to_lime(const time_point &tp) noexcept {
+    return lime::Timespec(duration(tp - sys_epoch).count());
   }
 
 private:
   const lime::RFStream &stream;
-  // time_point sys_epoch;
-  std::chrono::system_clock::time_point sys_epoch;
+  time_point sys_epoch;
 };
 
 void limeSuiteLogHandler(const lime::LogLevel level,
